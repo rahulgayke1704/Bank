e42f0c7c4d76ef3a4065cd5e4c8f87dd02889471
rahulgayke1704 <rahulgayke1704@gmail.com>
2025-11-01 12:50:53 +0530

    ***

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from . BankAccount import BankAccount
+from . CurrentAccount import CurrentAccount
+from . FixedDeposit import FixedDeposit
+from . SavingAccount import SavingAccount
+
+
+
+       #module(file.py)       # claas
+
+__all__=["BankAccount","CurrentAccount","FixedDeposit","SavingAccount"]
